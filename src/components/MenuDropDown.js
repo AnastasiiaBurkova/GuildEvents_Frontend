fad1fadc3f25eca8c686ca6830b1bbcc6ae077e2
ext-noora.turunen <ext-noora.turunen@accountorhr.fi>
2023-03-18 23:33:16 +0200

    Navbar and other style changes

---
+++ src/components/MenuDropDown.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { Link } from "react-router-dom";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { MenuDropDownStyle } from './MenuDropDownStyle';
+
+export default function MenuDropDown() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        sx={MenuDropDownStyle}
+        id="demo-positioned-button"
+        aria-controls={open ? 'demo-positioned-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon />}
+        
+      >
+        Menu
+      </Button>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <MenuItem onClick={handleClose} component={Link} to='/about'>About us</MenuItem>
+        <MenuItem onClick={handleClose} component={Link} to='/terms'>Terms and Conditions</MenuItem>
+      </Menu>
+    </div>
+  );
+}
